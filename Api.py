c0d81aaa6599e0887318ec3408cadf3bf703e761
vardaan-7 <vardaan.shukla7@gmail.com>
2026-01-10 22:37:28 +0530

    Phase 7B: Serve probabilistic ML predictions via FastAPI

---
+++ Api.py
@@ -0,0 +1,76 @@
+from fastapi import FastAPI
+import yfinance as yf
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+
+app = FastAPI(title="MarketLens API")
+
+@app.on_event("startup")
+def train_model():
+    global model, features
+
+    ticker = "ASHOKLEY.NS"
+    df = yf.download(ticker, period="5y", interval="1d")
+
+    if isinstance(df.columns, pd.MultiIndex):
+        df.columns = df.columns.droplevel(1)
+
+    df["Daily_Return"] = df["Close"].pct_change()
+    df["SMA_20"] = df["Close"].rolling(20).mean()
+    df["EMA_20"] = df["Close"].ewm(span=20, adjust=False).mean()
+
+    delta = df["Close"].diff()
+    gain = delta.clip(lower=0)
+    loss = -delta.clip(upper=0)
+
+    avg_gain = gain.rolling(14).mean()
+    avg_loss = loss.rolling(14).mean()
+    rs = avg_gain / avg_loss
+    df["RSI_14"] = 100 - (100 / (1 + rs))
+
+    df["Volatility_20"] = df["Daily_Return"].rolling(20).std()
+    df["Target"] = (df["Daily_Return"].shift(-1) > 0).astype(int)
+
+    features = ["SMA_20", "EMA_20", "RSI_14", "Volatility_20"]
+    df_model = df[features + ["Target"]].dropna()
+
+    X = df_model[features]
+    y = df_model["Target"]
+
+    model = LogisticRegression(max_iter=1000)
+    model.fit(X, y)
+
+
+# API Endpoint
+
+@app.get("/predict")
+def predict():
+    ticker = "ASHOKLEY.NS"
+    df = yf.download(ticker, period="40d", interval="1d")
+
+    if isinstance(df.columns, pd.MultiIndex):
+        df.columns = df.columns.droplevel(1)
+
+    df["Daily_Return"] = df["Close"].pct_change()
+    df["SMA_20"] = df["Close"].rolling(20).mean()
+    df["EMA_20"] = df["Close"].ewm(span=20, adjust=False).mean()
+
+    delta = df["Close"].diff()
+    gain = delta.clip(lower=0)
+    loss = -delta.clip(upper=0)
+
+    avg_gain = gain.rolling(14).mean()
+    avg_loss = loss.rolling(14).mean()
+    rs = avg_gain / avg_loss
+    df["RSI_14"] = 100 - (100 / (1 + rs))
+
+    df["Volatility_20"] = df["Daily_Return"].rolling(20).std()
+
+    latest = df[features].dropna().iloc[-1:]
+
+    prob_up = model.predict_proba(latest)[0][1]
+
+    return {
+        "ticker": ticker,
+        "probability_up": round(float(prob_up), 4)
+    }
